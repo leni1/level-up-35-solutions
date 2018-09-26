bd3ce573dfaf520069add0445fc2adb3df887346
Leni Kadali Mutungi <lenikmutungi@gmail.com>
2018-09-26 15:40:05 +0300

    ch: Add formatting to conform to PEP8
    Added formatting to conform to PEP8

--- tests/test_user_class.py
+++ tests/test_user_class.py
@@ -6,7 +6,9 @@ from models.user_class import User
 class UserTest(unittest.TestCase):
 
     def setUp(self):
-        self.user = User('chicken', 5, 'chick@chi.chi', 'somepassword', 'chicky')
+        self.user = User('chicken', 5,
+                         'chick@chi.chi', 'somepassword',
+                         'chicky')
 
     def test_user_has_logged_in_attribute(self):
         self.assertTrue(hasattr(User, 'logged_in'),
@@ -15,7 +17,8 @@ class UserTest(unittest.TestCase):
     def test_user_class_has_valid_properties(self):
         args = {'name', 'user_name', 'email', 'age', 'password'}
         for arg in args:
-            self.assertTrue(hasattr(self.user, arg), msg='{} argument is missing.'.format(arg))
+            self.assertTrue(hasattr(self.user, arg),
+                            msg='{} argument is missing.'.format(arg))
 
     def test_user_instance_valid(self):
         self.assertIsInstance(self.user, User)
@@ -28,7 +31,8 @@ class UserTest(unittest.TestCase):
         self.assertEqual(self.user.logged_in, True)
 
     def test_login_attribute_can_have_diff_values_per_user(self):
-        self.user_2 = User('chicken', 5, 'chick@chi.chi', 'somepassword', 'chicky')
+        self.user_2 = User('chicken', 5, 'chick@chi.chi',
+                           'somepassword', 'chicky')
         self.user_2.logged_in = True
         self.assertEqual(self.user.logged_in, False)
         self.assertEqual(self.user_2.logged_in, True)
