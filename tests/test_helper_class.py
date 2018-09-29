ddd3eeb482e51ab3f5a9deeb6d9c828dc1b63547
Leni Kadali Mutungi <lenikmutungi@gmail.com>
2018-09-29 12:42:50 +0300

    feat: Add further tests for the helper class
    Added further tests for the helper class

--- tests/test_helper_class.py
+++ tests/test_helper_class.py
@@ -17,6 +17,26 @@ class UserTest(unittest.TestCase):
         for attr in attrs:
             self.assertIn(attr, attrs, msg='{} attribute missing.'.format(attr))
 
+    def test_check_password_too_short(self):
+        wrong = self.helper.check_password('som')
+        self.assertEqual(wrong, 'Password too short.')
+
+    def test_check_password_no_digit(self):
+        wrong = self.helper.check_password('some')
+        self.assertEqual(wrong, 'Password has no digit.')
+
+    def test_check_password_no_uppercase(self):
+        wrong = self.helper.check_password('som98')
+        self.assertEqual(wrong, 'Password has no capital letter.')
+
+    def test_check_password_no_symbol(self):
+        wrong = self.helper.check_password('s98A')
+        self.assertEqual(wrong, 'Password has no special character.')
+
+    def test_check_password_success(self):
+        correct = self.helper.check_password('s98A$')
+        self.assertTrue(correct)
+
     def test_check_email_fail(self):
         invalid = self.helper.check_email('somemail')
         self.assertNotEqual(invalid, True)
@@ -27,9 +47,37 @@ class UserTest(unittest.TestCase):
         self.assertEqual(correct, True)
         self.assertIsInstance(correct, bool)
 
-    def test_register(self):
-        register_user = self.helper.register_user('som', 'som', 'some@mail.com', 'Abcd8^', 1)
-        self.assertEqual(register_user, 'Username or name too short.')
-    
-    
-        
+    def test_register_name_short(self):
+        name1 = self.helper.register_user('som', 'som', 'some@mail.com', 'Abcd8^', 1)
+        name2 = self.helper.register_user('som', 'some', 'some@mail.com', 'Abcd8^', 1)
+        self.assertEqual(name1, 'Username or name too short.')
+        self.assertEqual(name2, 'Username or name too short.')
+
+
+    def test_register_name_same(self):
+        name1 = self.helper.register_user('some', 'some', 'some@mail.com', 'Abcd8^', 1)
+        name2 = self.helper.register_user('SOME', 'some', 'some@mail.com', 'Abcd8^', 1)
+        self.assertEqual(name1, 'Name and username cannot be the same.')
+        self.assertEqual(name2, 'Name and username cannot be the same.')
+
+    def test_register_invalid_age_cases(self):
+        invalid_1 = self.helper.register_user('some', 'some', 'some@mail.com', 'Abcd8^', '1')
+        invalid_2 = self.helper.register_user('some', 'some', 'some@mail.com', 'Abcd8^', 0)
+        self.assertEqual(invalid_1, 'Invalid age.')
+        self.assertEqual(invalid_2, 'Invalid age.')
+
+    def test_invalid_email_valid_password(self):
+        invalid = self.helper.register_user('Jack', 'someJack', 'some.com', 'Abcd8^', 1)
+        self.assertFalse(invalid)
+
+    def test_invalid_email_invalid_password(self):
+        invalid = self.helper.register_user('Jack', 'someJack', 'some.com', 'Abcd8', 1)
+        self.assertFalse(invalid)
+
+    def test_valid_password_invalid_email(self):
+        invalid = self.helper.register_user('Jack', 'someJack', 'some.com', 'Abcd8^', 1)
+        self.assertFalse(invalid)
+
+    def test_valid_email_valid_password(self):
+        valid = self.helper.register_user('Jack', 'someJack', 'some@mail.com', 'Abcd8^', 1)
+        self.assertTrue(valid)
